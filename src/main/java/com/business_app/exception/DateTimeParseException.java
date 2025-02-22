5141ee2cbc49251aaabef848531b8b850b11a148
Olga Dombrovskaya <olgaekb26@gmail.com>
2025-02-22 17:39:55 +0000

    added exception

---
+++ src/main/java/com/business_app/exception/DateTimeParseException.java
@@ -0,0 +1,22 @@
+package com.business_app.exception;
+
+public class DateTimeParseException extends Exception{
+    public DateTimeParseException() {
+    }
+
+    public DateTimeParseException(String message) {
+        super(message);
+    }
+
+    public DateTimeParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DateTimeParseException(Throwable cause) {
+        super(cause);
+    }
+
+    public DateTimeParseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
