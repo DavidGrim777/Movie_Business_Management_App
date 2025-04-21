617913af29e8987673dddf738b344f537c95d34b
David Grimberg <davidgrim676@gmail.com>
2025-04-21 12:16:40 +0200

    added enum ContractStatus

---
+++ src/main/java/movie/business/app/enums/ContractStatus.java
@@ -0,0 +1,8 @@
+package movie.business.app.enums;
+
+public enum ContractStatus {
+    PENDING,     // Ожидает подписания
+    ACTIVE,      // Активный контракт
+    COMPLETED,   // Завершённый контракт
+    CANCELLED    // Отменённый контракт
+}
